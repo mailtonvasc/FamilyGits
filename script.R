da8ec0f4d6fb9898c016ee5845312dc98f02788f
mailtonvasc <59204036+mailtonvasc@users.noreply.github.com>
2025-11-26 15:07:26 +0000

    Setting everything up

---
+++ script.R
@@ -0,0 +1,8 @@
+# 26/11/2025
+# MV: Create a vector from 1 to 15
+v <- 1:15
+
+# MV: Calculate the sum of all elements in the vector
+sum_v <- sum(v)  # Sum of numbers from 1 to 15
+
+# ---- Add your own calculation below and annotate it! ----
